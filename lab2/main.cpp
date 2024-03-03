bf045b7168fecc44b2d00199ae306124d867977d
Andy Loftus <aloftus@gmail.com>
2024-03-03 21:37:43 +0000

    add lab 2

---
+++ lab2/main.cpp
@@ -0,0 +1,36 @@
+/* to use this, 
+     - write a form that calls this program with 'action=' in the <form> tag
+     - call getPostData() 
+     - use getField(string) to access form information 
+
+*/
+#include <iostream>
+#include "htmlform.h"
+
+using namespace std;
+
+int main()
+{
+  /// complete the http header
+  cout << "Content-type: text/html\n\n";
+
+  /// build a form object
+  HTMLform monty;
+  monty.getPostData(); /// reads the form and inits the object
+
+  /// Show the debugging (optional)
+  monty.debug();
+
+  /// the output ('name' is in the html form that started this)
+  cout << "Welcome, " << monty.getField("name") << "!";
+  cout << "Welcome again, " << monty.getField("name") << "!";
+  cout << "Welcome once more, " << monty.getField("name") << "!";
+
+
+  /// access another field ('color' is in the html form)
+  cout << "Your favorite color is " << monty.getField("color") << "!";
+  cout << "The airspeed is " << monty.getField("airspeed") << " mph. ";
+
+ 
+  return 0;
+}
---
+++ lab2/runme.cgi
@@ -0,0 +1,4 @@
+<hr />Version 1 from 9/23/15, written by Ken Urban in class <br/>
+Version 2 from 2/11/16, modified by Ken Urban <br />
+Version 3 from 2/6/18, modified by Ken Urban <br />
+Data in:[] <br/><br /><center>Table stored:<br /><table border='1'></table></center><hr />Welcome, !Welcome again, !Welcome once more, !Your favorite color is !The airspeed is  mph. 
